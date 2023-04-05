6e062f3a6a7d4cc68cfbd4a8f9ccb8a2380bbe7c
Ruslan379 <arnov@i.ua>
2023-04-05 19:47:37 +0300

    Start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import whel from 'images/roulette-wheel.png';
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101'
+        }}>
+      <img
+        style={{
+          marginBottom: '40px',
+          
+        }}
+            alt={whel}
+            src={whel}
+            width="200"
+          />
+      <div>
+        Easy Start in WWW (What? Where? When?) 
+      </div>
     </div>
   );
 };
