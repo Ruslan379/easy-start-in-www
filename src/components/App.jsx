fdf9686940cf8a1ee4a2b5d37bc5bbe333badcfe
Ruslan379 <arnov@i.ua>
2023-04-11 22:17:51 +0300

    JS-1_1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,20 @@
 import whel from 'images/roulette-wheel.png';
 
+//! ---------------------------------------------------------------------------------------
+const valueA = "5";
+console.log("valueA:", valueA); // 5
+console.log("Number(valueA):", Number(valueA)); // 5
+console.log("typeof Number(valueA):", typeof Number(valueA)); // "number"
+console.log("typeof Number(NaN):", typeof Number(NaN)); // "number"
+console.log("Number(NaN):", Number(NaN)); // "number"
+//! ---------------------------------------------------------------------------------------
+console.log("--------------------------------------------"); 
+const valueB = "random string";
+console.log("valueB:", valueB); // random string
+console.log("Number(valueB)", Number(valueB)); // NaN
+console.log("typeof Number(valueB)", typeof Number(valueB)); // "number"
+//! ---------------------------------------------------------------------------------------
+
 export const App = () => {
   return (
     <div
