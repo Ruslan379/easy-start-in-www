48bc90732f8f1bf0b79ab3046aa5058c92549bec
Ruslan379 <arnov@i.ua>
2023-04-14 17:40:09 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,43 @@
 import whel from 'images/roulette-wheel.png';
 
 //! ---------------------------------------------------------------------------------------
-const valueA = "5";
-console.log("valueA:", valueA); // 5
-console.log("Number(valueA):", Number(valueA)); // 5
-console.log("typeof Number(valueA):", typeof Number(valueA)); // "number"
-console.log("typeof Number(NaN):", typeof Number(NaN)); // "number"
-console.log("Number(NaN):", Number(NaN)); // NaN
+// const valueA = "5";
+// console.log("valueA:", valueA); // 5
+// console.log("Number(valueA):", Number(valueA)); // 5
+// console.log("typeof Number(valueA):", typeof Number(valueA)); // "number"
+// console.log("typeof Number(NaN):", typeof Number(NaN)); // "number"
+// console.log("Number(NaN):", Number(NaN)); // NaN
+//! ---------------------------------------------------------------------------------------
+// console.log("--------------------------------------------");
+// const valueB = "random string";
+// console.log("valueB:", valueB); // random string
+// console.log("Number(valueB)", Number(valueB)); // NaN
+// console.log("typeof Number(valueB)", typeof Number(valueB)); // "number"
+//! ---------------------------------------------------------------------------------------
+
+// console.log((Math.random() * 100).toFixed(0)) // псевдослучайное число от 1 до 100
+// console.log((Math.random() * (100 - 1) + 1).toFixed(0)); // псевдослучайное число от 1 до 100
 //! ---------------------------------------------------------------------------------------
 console.log("--------------------------------------------"); 
-const valueB = "random string";
-console.log("valueB:", valueB); // random string
-console.log("Number(valueB)", Number(valueB)); // NaN
-console.log("typeof Number(valueB)", typeof Number(valueB)); // "number"
+const cars = ['Audio', 'Volvo', 'Nissan', 'BMW', 'Mazda', 'Toyota'];
+const cars1 = cars
+
+const arrayCutter = function (array) {
+  array.splice(3, array.length - 1);
+  console.log(cars1);
+  return;
+};  
+arrayCutter(cars);
+
+
 //! ---------------------------------------------------------------------------------------
-console.log((Math.random() * 100).toFixed(0)) // псевдослучайное число от 1 до 100
-console.log((Math.random() * (100 - 1) + 1).toFixed(0)); // псевдослучайное число от 1 до 100
+// const post = (
+//   <h2>Post Header</h2>
+//   <p>Post text</p>
+// );
+//! ---------------------------------------------------------------------------------------
+
+
 
 export const App = () => {
   return (
