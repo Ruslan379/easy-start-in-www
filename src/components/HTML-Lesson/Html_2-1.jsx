5e4e2080e357aa413b8c9c005d39e3b536b382d5
Ruslan379 <arnov@i.ua>
2023-04-18 21:29:02 +0300

    Html_2-1___14

--- src/components/HTML-Lesson/Html_2-1.jsx
+++ src/components/HTML-Lesson/Html_2-1.jsx
@@ -10,7 +10,6 @@ import css from './Html_2-1.module.css';
 export const SectionText = () => {
     const [isSuccess, setIsSuccess] = useState(false);
 
-
     const sectionText1 = (
         <section className={css.section}>
             <h1 className={css.sectionTitle}>Section title</h1>
@@ -36,21 +35,22 @@ export const SectionText = () => {
             <br />
             <button
                 className={
-                    `${css.alert} ${isSuccess
+                    `${css.buttonAlert} ${css.alert} ${
+                        isSuccess
                         ?
                         `${css.success} ${css.successBackgroundColor}`
                         :
                         // css.error
                         `${css.error} ${css.errorBackgroundColor}`
-                    }
+                        }
                     `
                 }
                 onClick={toggleTrigger}
             >
                 {isSuccess ? "Пополнение счёта выполнено" : "Пополнение счёта НЕ выполнено"}
             </button>
-
             {/* <p className={`${css.alert} ${css.success}`}>Пополнение счёта выполнено</p> */}
+            
             <p
                 className={`${css.alert} ${isSuccess ? css.success : css.error}`}
             >
