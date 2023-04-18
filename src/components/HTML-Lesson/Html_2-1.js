0eb79eea3cfa24b05e101708b22b28f96f04742f
Ruslan379 <arnov@i.ua>
2023-04-18 20:13:16 +0300

    Html_2-1___11

--- src/components/HTML-Lesson/Html_2-1.js
+++ src/components/HTML-Lesson/Html_2-1.js
@@ -1,10 +1,14 @@
-
+import {
+    useState,
+    // useEffect
+} from 'react';
 
 import css from './Html_2-1.module.css';
 
 
 //-----------------------------------------------------
 export const SectionText = () => {
+    const [isSuccess, setIsSuccess] = useState(false);
 
 
     const sectionText1 = (
@@ -12,6 +16,14 @@ export const SectionText = () => {
             <h1 className={css.sectionTitle}>Section title</h1>
         </section>
     );
+
+    // let isSuccess
+    // isSuccess = true;
+    // isSuccess = false;
+
+    const toggleTrigger = () => {
+        setIsSuccess(!isSuccess);
+    }
 
 
     return (
@@ -21,6 +33,23 @@ export const SectionText = () => {
             <section className={css.section}>
                 <h1 className={`${css.sectionTitle} ${css.fontSizeColorTitle}`}>Section title</h1>
             </section>
+            <br />
+            <button
+                className={`${css.alert} ${isSuccess ? css.success : css.error}`}
+                onClick={toggleTrigger}
+            >
+                {isSuccess ? "Пополнение счёта выполнено" : "Пополнение счёта НЕ выполнено"}
+            </button>
+
+            {/* <p className={`${css.alert} ${css.success}`}>Пополнение счёта выполнено</p> */}
+            <p
+                className={`${css.alert} ${isSuccess ? css.success : css.error}`}
+            >
+                {/* Пополнение счёта выполнено */}
+                {isSuccess ? "Пополнение счёта выполнено" : "Пополнение счёта НЕ выполнено"}
+            </p>
+            <p className={`${css.alert} ${css.warning}`}>Внимание, будут изменены тарифы</p>
+            <p className={`${css.alert} ${css.error}`}>Ошибка проведения транзакции</p>
         </>
     )
 }
