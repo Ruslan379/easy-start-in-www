83b6d1fb03c3c0319aa77a9becd7133f2bff8c20
Ruslan379 <arnov@i.ua>
2023-04-19 18:49:17 +0300

    Html_2-4___23

--- src/components/HTML-Lesson/Html_2-3_Question_Julia.jsx
+++ src/components/HTML-Lesson/Html_2-3_Question_Julia.jsx
@@ -10,28 +10,28 @@ export const HtmlQuestion3Julia = () => {
             <a href="https://www.google.com.ua">Читайте наш блог!</a>
                 <ul className={css.socialLinks}>
                     <li className={css.socialItems}>
-                        <p className={css.socialText}>Twitter</p>
+                        {/* <p className={css.socialText}>Twitter</p> */}
                         <a href="https://www.google.com.ua">Twitter-1</a>
                         <br/>
-                        <a href="https://www.google.com.ua">Twitter-2</a>
+                        {/* <a href="https://www.google.com.ua">Twitter-2</a> */}
                         <br/>
-                        <a href="https://www.google.com.ua">Twitter-3</a>
+                        {/* <a href="https://www.google.com.ua">Twitter-3</a> */}
                     </li>
                     <li>
-                        <p className={css.socialText}>Instagram</p>
+                        {/* <p className={css.socialText}>Instagram</p> */}
                         <a href="https://www.google.com.ua">Instagram-1</a>
                         <br/>
-                        <a href="https://www.google.com.ua">Instagram-2</a>
+                        {/* <a href="https://www.google.com.ua">Instagram-2</a> */}
                         <br/>
-                        <a href="https://www.google.com.ua">Instagram-3</a>
+                        {/* <a href="https://www.google.com.ua">Instagram-3</a> */}
                     </li>
                     <li>
-                        <p className={css.socialText}>Facebook</p>
+                        {/* <p className={css.socialText}>Facebook</p> */}
                         <a href="https://www.google.com.ua">Facebook-1</a>
                         <br/>
-                        <a href="https://www.google.com.ua">Facebook-2</a>
+                        {/* <a href="https://www.google.com.ua">Facebook-2</a> */}
                         <br/>
-                        <a href="https://www.google.com.ua">Facebook-3</a>
+                        {/* <a href="https://www.google.com.ua">Facebook-3</a> */}
                     </li>
                 </ul>
         </div>
