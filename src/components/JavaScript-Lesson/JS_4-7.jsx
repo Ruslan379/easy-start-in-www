59db22e2069b54f5a17bb08b4cf1f88a2b97459f
Ruslan379 <arnov@i.ua>
2023-04-20 22:21:38 +0300

    JavaScript_4-7___32

--- src/components/JavaScript-Lesson/JS_4-7.jsx
+++ src/components/JavaScript-Lesson/JS_4-7.jsx
@@ -1,6 +1,6 @@
 
 
-// import css from './JS_4-7.module.css';
+import css from './JS_4-7.module.css';
 
 
 //-----------------------------------------------------
@@ -11,18 +11,18 @@ export const JS_4_7 = () => {
     console.log("Псевдомассив arguments у стрелочных функций:");
     
     const add = (...args) => {
-        console.log("...args:", args); // [1, 2, 3]
+        console.log("...args -->:", args); // [1, 2, 3]
         return args
     };
 
-    const argsArr = add(1, 2, 3)
+    const argsArr = add(1, 2, 3, 4, 5)
     console.log("...args:", argsArr);  // [1, 2, 3]
 
     //! IIFE - работает
     // ((...args) => {
     //     console.log("...args:", args); // [1, 2, 3]
     //     return args
-    // })(1, 2, 3);
+    // })(1, 2, 3, 4, 5);
     
 
 
@@ -30,23 +30,16 @@ export const JS_4_7 = () => {
         <>
             <p style={{ color: "tomato" }}>{`<---- ${"JS_4-7"} ---->`}</p>
             {/* Псевдомассив arguments у стрелочных функцийв */}
-
-            {/* <ul className={css.rgbList}>
-                {rgbArr.map(({ red, green, blue }, index,) => (
+            <ul className={css.rgbList}>
+                {argsArr.map((item, index,) => (
                     <li key={index} className={css.rgbItem}>
                         <h3>{`----------- ${index} -----------`}</h3>
-                        <p className={css.rgbTextRed}>
-                            {`Red: ${red}`}
-                        </p>
                         <p className={css.rgbTextGreen}>
-                            {`Green: ${green}`}
-                        </p>
-                        <p className={css.rgbTextBlue}>
-                            {`Blue: ${blue}`}
+                            {`item: ${item}`}
                         </p>
                     </li>
                 ))}
-            </ul> */}
+            </ul>
         </>
     )
 }
