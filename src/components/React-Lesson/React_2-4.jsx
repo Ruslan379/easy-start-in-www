1147e484d0b46f844e458075f08f99b4d291330d
Ruslan379 <arnov@i.ua>
2023-04-22 15:34:40 +0300

    React_2-4___40

--- src/components/React-Lesson/React_2-4.jsx
+++ src/components/React-Lesson/React_2-4.jsx
@@ -8,6 +8,7 @@ import React, { Component } from 'react';
 export const React_2_4 = () => {
     console.log("React_2-4:");
 
+    //! Контролируемые элементы
     class React24 extends Component {
         state = {
         inputValue: "",
@@ -27,12 +28,60 @@ export const React_2_4 = () => {
         
     }
 
+
+    //! Сложные формы
+    class SignUpForm extends Component {
+        state = {
+        login: "",
+        };
+
+    // Отвечает за обновление состояния
+        handleChange = e => {
+            this.setState({ login: e.target.value });
+        };
+
+        // Вызывается при отправке формы
+        handleSubmit = evt => {
+            evt.preventDefault();
+            console.log(`Signed up as: ${this.state.login}`);
+
+        // Проп который передается форме для вызова при сабмите
+        this.props.onSubmit({ ...this.state });
+        };
+
+        render() {
+            const { login } = this.state;
+
+            return (
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Name
+                    <input
+                        type="text"
+                        placeholder="Enter login"
+                        value={login}
+                        onChange={this.handleChange}
+                    />
+                    </label>
+
+                    <button type="submit">Sign up as {login}</button>
+                </form>
+            );
+        }
+    }
+
+    // ReactDOM.render(
+    //     <SignUpForm onSubmit={values => console.log(values)} />,
+    //     document.getElementById("root")
+    // );
+
     
     return (
         <>
             <p style={{ color: "tomato" }}>{`<---- ${"React_2-4"} ---->`}</p>
             <React24 />
             <br />
+            <SignUpForm />
         </>
     )
 }
