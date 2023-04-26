5aa5a752142bd333dec6a02424d9009957bb6168
Ruslan379 <arnov@i.ua>
2023-04-26 17:48:11 +0300

    Html_3-6___49

--- src/components/HTML-Lesson/Html_3-6.jsx
+++ src/components/HTML-Lesson/Html_3-6.jsx
@@ -3,7 +3,7 @@
 //     // useEffect
 // } from 'react';
 
-// import css from './Html_3-6.module.css';
+import css from './Html_3-6.module.css';
 
 
 //-----------------------------------------------------
@@ -17,6 +17,12 @@ export const HTML_36 = () => {
         <>
             <p style={{ color: "tomato" }}>{`<---- ${"HTML_3-6"} ---->`}</p>
             
+            <ul className={css.menu}>
+                <li className={css.item}><a href="https://www.google.com.ua" className={css.link}>Home page</a></li>
+                <li className={css.item}><a href="https://www.google.com.ua" className={css.link}>Blog</a></li>
+                <li className={css.item}><a href="https://www.google.com.ua" className={css.link}>Podcasts</a></li>
+                <li className={css.item}><a href="https://www.google.com.ua" className={css.link}>Contacts</a></li>
+            </ul>
             {/* <p className={`${css.alert} ${css.warning}`}>Внимание, будут изменены тарифы</p>
             <p className={`${css.alert} ${css.error}`}>Ошибка проведения транзакции</p>
             <p id={css.textId}>Текст с селектором id (#)</p> */}
