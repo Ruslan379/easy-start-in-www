338d70d6d15de25a4187d15d6590285910c6219b
Ruslan379 <arnov@i.ua>
2023-05-04 16:59:10 +0300

    JavaScript_6-11___73

--- src/components/JavaScript-Lesson/JS_611.jsx
+++ src/components/JavaScript-Lesson/JS_611.jsx
@@ -1,19 +1,77 @@
 
 
-// import css from './JS_611.module.css';
+import css from './JS_611.module.css';
 
 
 //-----------------------------------------------------
 export const JS_611 = () => {
     console.log("JS_6-11:");
 
-    //! this в методе объекта
+    //! Навигация по DOM:
+    console.log("Навигация по DOM:");
 
+    console.log("document:", document);
+
+    const body = document.body;
+    console.log("body:", body);
+    
+    const list = body.firstElementChild;
+    console.log("list:", list);
+    
+    const firstListItem = list.firstElementChild;
+    console.log("firstListItem:", firstListItem);
+    
+    const listItems = list.children;
+    console.log("listItems:", listItems);
+
+    //! Поиск элементов
+    console.log("Поиск элементов:");
+
+    // <ul id="menu">
+    //     <li className={css.menuItem}>home</li>
+    //     <li className={css.menuItem}>about</li>
+    //     <li className={css.menuItem}>gallery</li>
+    //     <li>blog</li>
+    // </ul>
+
+    const listWithId = document.querySelector('#menu');
+    // listWithId.style.textTransform = 'uppercase';
+    // listWithId.style.fontSize = '24px';
+    console.log("listWithId:", listWithId);
+    
+    const listWithClass = document.querySelector('.menu');
+    console.log("listWithClass:", listWithClass);
+    
+    const menuItemsByTagName = document.querySelectorAll("li");
+    console.log("menuItemsByTagName:", menuItemsByTagName);
+    
+    const menuItemsByClass = document.querySelectorAll(".menu-item");
+    console.log("menuItemsByClass:", menuItemsByClass);
+    
+    const firstMenuItem = document.querySelector(".menu-item");
+    // firstMenuItem.style.color = 'tomato';
+    console.log("firstMenuItem:", firstMenuItem);
 
     return (
         <>
             <p style={{ color: "tomato" }}>{`<---- ${"JS_6-11"} ---->`}</p>
-            {/* Продвинутый reduce */}
+            {/* Навигация по DOM */}
+            <h3 style={{ color: "blue" }}>{`Навигация по DOM`}</h3>
+            <ul>
+                <li>First item</li>
+                <li>Second item</li>
+                <li>Third item</li>
+                <li>Fourth item</li>
+                <li>Fifth item</li>
+            </ul>
+            {/* Поиск элементов */}
+            <h3 style={{ color: "blue" }}>{`Поиск элементов`}</h3>
+            <ul id="menu" className={css.menuList}>
+                <li className={css.menuItem}>home</li>
+                <li className={css.menuItem}>about</li>
+                <li className={css.menuItem}>gallery</li>
+                <li>blog</li>
+            </ul>
             
         </>
     )
