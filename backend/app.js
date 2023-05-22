2f512e79b7af87bfd890dc2c858e0d559fd3101c
Ruslan379 <arnov@i.ua>
2023-05-22 17:18:31 +0300

    Node.js_1-1___117

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,9 @@ const fsPromises = require("fs/promises");
 const fs = require("fs").promises;
 
 //---------------------------------------------------------------------------
-//! require("fs")
+//! readFile --> require("fs")
 const appFilesOld = fsOld.readFile("./trainingModule/files/files.txt", (error, data) => {
-    console.log("fsOld --> appFilesOld:".yellow);
+    console.log("fsOld.readFile --> appFilesOld:".yellow);
     console.log("error:".red, error);
     console.log("data:".green, data);
     console.log("");
@@ -13,10 +13,10 @@ const appFilesOld = fsOld.readFile("./trainingModule/files/files.txt", (error, d
 
 
 
-//! require("fs/promises")
+//! readFile --> require("fs/promises")
 const appFilesPromises = fsPromises.readFile("./trainingModule/files/files.txt")
     .then(data => {
-        console.log("fsPromises --> appFilesPromises:".yellow);
+        console.log("fsPromises.readFile --> appFilesPromises:".yellow);
         console.log("data:".green, data);
         console.log("");
     })
@@ -24,10 +24,10 @@ const appFilesPromises = fsPromises.readFile("./trainingModule/files/files.txt")
 
 
 
-//! async/await
+//! readFile --> async/await
 const ReadFile = async () => {
     const buffer = await fs.readFile("./trainingModule/files/files.txt");
-    console.log("fs --> async/await -> ReadFile:".yellow);
+    console.log("fs.readFile --> async/await -> ReadFile:".yellow);
     console.log("buffer:".green, buffer);
 
     const textToString = buffer.toString();
@@ -35,20 +35,50 @@ const ReadFile = async () => {
 
     console.log("");
 };
-
 const asyncAwaitReadFile = ReadFile();
 
 
 
-//! async/await by "utf-8"
+//! readFile --> async/await by "utf-8"
 const ReadFileUtf8 = async () => {
     const textUtf8 = await fs.readFile("./trainingModule/files/files.txt", "utf-8");
-    console.log("fs --> async/await -> ReadFileUtf8:".yellow);
+    console.log("fs.readFile --> async/await -> ReadFileUtf8:".yellow);
     console.log("textUtf8:".bgGreen.black, textUtf8.red);
     console.log("");
 };
-
 const asyncAwaitReadFileUtf8 = ReadFileUtf8();
+
+
+//! appendFile --> async/await
+const addText = async () => {
+    const resultAppendFile = await fs.appendFile(
+        "./trainingModule/files/files.txt",
+        "\n++ Add this text to the end ++"
+    );
+    const textUtf8 = await fs.readFile("./trainingModule/files/files.txt", "utf-8");
+    console.log("fs.appendFile --> async/await -> addText:".yellow);
+    console.log("resultAppendFile:".bgCyan.black, resultAppendFile); //! undefined
+    console.log("textUtf8:".bgGreen.black, textUtf8.red);
+    console.log("");
+};
+const appendText = addText();
+
+
+//! writeFile --> async/await
+const replaceText = async () => {
+    const resultWriteFile = await fs.writeFile(
+        "./trainingModule/files/files2.txt",
+        "-- Replace ALL text in this files --"
+    );
+    const textUtf8 = await fs.readFile("./trainingModule/files/files2.txt", "utf-8");
+    console.log("fs.appendFile --> async/await -> addText:".yellow);
+    console.log("resultWriteFile:".bgCyan.black, resultWriteFile); //! undefined
+    console.log("textUtf8:".bgGreen.black, textUtf8.red);
+    console.log("");
+};
+const writeText = replaceText();
+
+
 
 
 module.exports = {
@@ -56,4 +86,6 @@ module.exports = {
     appFilesPromises,
     asyncAwaitReadFile,
     asyncAwaitReadFileUtf8,
+    appendText,
+    writeText,
 }
