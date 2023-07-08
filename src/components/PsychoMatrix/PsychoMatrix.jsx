399aba12283b7f84c48c3ec950f797a64fa308ba
Ruslan379 <arnov@i.ua>
2023-07-09 01:13:26 +0300

    matrix - 135

--- src/components/PsychoMatrix/PsychoMatrix.jsx
+++ src/components/PsychoMatrix/PsychoMatrix.jsx
@@ -19,7 +19,8 @@ export const PsychoMatrix = () => {
     const [day, setDay] = useState(0);
     const [month, setMonth] = useState(0);
     const [year, setYear] = useState(0);
-    const [dateOfBirth, setDateOfBirth] = useState({});
+
+    const [dateOfBirthObj, setDateOfBirthObj] = useState({});
 
     const [firstAddNumber, setFirstAddNumber] = useState(0);
     const [secondAddNumber, setSecondAddNumber] = useState(0);
@@ -39,7 +40,18 @@ export const PsychoMatrix = () => {
     // const [quantity9, setQuantity9] = useState(0);
     // const [quantity0, setQuantity0] = useState(0);
 
+    //*------------------ ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ ------------------
+    //! Преобразование числа в строку а затем в массив строк:
+    const convertNumberToArray = (arr) => arr.toString().split("");
 
+    //! Преобразование всех элементов массива (строк) в ЧИСЛО и СУММИРОВАНИЕ их:
+    const convertToNumberAndSum = (arr) => arr.reduce((accumulator, currentValue) => accumulator + Number(currentValue), 0);
+
+    //! Преобразование всех элементов массива (строк) в ЧИСЛО и запись в новый массив:
+    const convertToNumber = (arr) => arr.map(element => Number(element));
+
+
+    //*__________________ ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ __________________
 
     const handleChangeDay = evt => {
         setDay(evt.target.value);
@@ -60,57 +72,71 @@ export const PsychoMatrix = () => {
         evt.preventDefault();
         const form = evt.currentTarget;
         console.log('Вызывается при отправке формы:');
+        console.log(`SUBMIT--> День:${day}, Месяц:${month}, Год:${year}`); //!
 
         //todo OLD
         // setDay(form.elements.day.value);
         // setMonth(form.elements.month.value);
         // setYear(form.elements.year.value);
-        // setDateOfBirth({
+        // setdateOfBirthObj({
         //     day: form.elements.day.value,
         //     month: form.elements.month.value,
         //     year: form.elements.year.value
         // });
 
-        //! NEW --> setDateOfBirth- объект даты рождения
-        setDateOfBirth({
+        //! Создаем объект даты рождения --> dateOfBirthObj
+        setDateOfBirthObj({
             day,
             month,
             year
         });
-        console.log(`SUBMIT--> День:${day}, Месяц:${month}, Год:${year}`); //!
 
+
+        //todo OLD
         // //! Преобразуем все стейты в строки:
         // const dayAsString = day.toString();
         // const monthAsString = month.toString();
         // const yearAsString = year.toString();
 
+        //todo OLD
         // //! Преобразуем все строки в массивы:
         // const dayAsArray = dayAsString.split("");
         // const monthAsArray = monthAsString.split("");
         // const yearAsArray = yearAsString.split("");
 
-        //! Преобразуем все стейты в строки а затем в массивы:
-        const dayAsArray = day.toString().split("");
-        const monthAsArray = month.toString().split("");
-        const yearAsArray = year.toString().split("");
+        //todo OLD
+        //! Преобразуем все стейты в строки а затем в массивы строк:
+        // const dayAsArray = day.toString().split(""); 
+        // const monthAsArray = month.toString().split(""); 
+        // const yearAsArray = year.toString().split(""); 
+
+        //! Преобразуем все стейты в строки а затем в массивы строк:
+        const dayAsArray = convertNumberToArray(day);
+        const monthAsArray = convertNumberToArray(month);
+        const yearAsArray = convertNumberToArray(year);
+
+        // console.log("dayAsArray:", dayAsArray); //!
+        // console.log("monthAsArray:", monthAsArray); //!
+        // console.log("yearAsArray:", yearAsArray); //!
 
 
-        console.log("dayAsArray:", dayAsArray); //!
-        console.log("monthAsArray:", monthAsArray); //!
-        console.log("yearAsArray:", yearAsArray); //!
-
-
-        //! "Сливаем" все массивы dayAsArray, monthAsArray, yearAsArray в один массив allAsArray:
-        const allAsArray = [...dayAsArray, ...monthAsArray, ...yearAsArray]
-        console.log("allAsArray:", allAsArray); //!
-        //! Преобразовываем все элементы массива allAsArray в число и записываем в новый массив allAsArrayNumber:
-        // const allAsArrayNumber = allAsArray.map(element => Number(element));
-        // console.log("allAsArrayNumber:", allAsArrayNumber); //!
+        //! "Сливаем" все массивы: dayAsArray, monthAsArray, yearAsArray в один массив dateOfBirthAsArray:
+        const dateOfBirthAsArray = [
+            ...dayAsArray,
+            ...monthAsArray,
+            ...yearAsArray
+        ];
+        // console.log("dateOfBirthAsArray:", dateOfBirthAsArray); //!
+        //! Преобразовываем все элементы массива dateOfBirthAsArray (строки) в число и записываем в новый массив dateOfBirthAsArrayNumber:
+        // const dateOfBirthAsArrayNumber = dateOfBirthAsArray.map(element => Number(element)); //todo OLD
+        const dateOfBirthAsArrayNumber = convertToNumber(dateOfBirthAsArray);
+        console.log("dateOfBirthAsArrayNumber:", dateOfBirthAsArrayNumber); //!
 
 
         //! ----------- firstAddNumber I-----------:
-        //! Суммируем цифры массива allAsArray (дата рождения) и записываем в стейт setFirstAddNumber:
-        const forFirstAddNumber = allAsArray.reduce((accumulator, currentValue) => accumulator + Number(currentValue), 0);
+        //! Суммируем ВСЕ цифры массива dateOfBirthAsArray (дата рождения) и записываем в стейт setFirstAddNumber:
+        // const forFirstAddNumber = dateOfBirthAsArray.reduce((accumulator, currentValue) => accumulator + Number(currentValue), 0); //todo OLD
+        const forFirstAddNumber = convertToNumberAndSum(dateOfBirthAsArray);
         console.log("forFirstAddNumber:", forFirstAddNumber); //!
         setFirstAddNumber(forFirstAddNumber);
 
@@ -151,9 +177,9 @@ export const PsychoMatrix = () => {
         console.log("thirdAddNumberArray III:", thirdAddNumberArray); //!
         console.log("fourthAddNumberArray IV:", fourthAddNumberArray); //!
 
-        //! "Сливаем" все массивы стейтов ****AddNumberAsArray и Дня рождения (allAsArray) в один массив allAddNumberAsArray:
+        //! "Сливаем" все массивы стейтов ****AddNumberAsArray и Дня рождения (dateOfBirthAsArray) в один массив allAddNumberAsArray:
         const allArray = [
-            ...allAsArray,
+            ...dateOfBirthAsArray,
             ...firstAddNumberArray,
             ...secondAddNumberArray,
             ...thirdAddNumberArray,
@@ -165,7 +191,7 @@ export const PsychoMatrix = () => {
         const allNumberAsArrayNumber = allArray.map(element => Number(element));
         console.log("allNumberAsArrayNumber:", allNumberAsArrayNumber); //!
 
-        //! Подсчитываем количество цифр в массиве allAsArrayNumber и записывем эти данные в объект countObj:
+        //! Подсчитываем количество цифр в массиве dateOfBirthAsArrayNumber и записывем эти данные в объект countObj:
         const count = {};
 
         for (let i = 0; i < allNumberAsArrayNumber.length; i++) {
@@ -182,7 +208,7 @@ export const PsychoMatrix = () => {
         setMonth(0);
         setYear(0);
 
-        // console.log("dateOfBirth:", dateOfBirth);
+        // console.log("dateOfBirthObj:", dateOfBirthObj);
         // evt.currentTarget.reset();
         form.reset();
     };
@@ -191,7 +217,7 @@ export const PsychoMatrix = () => {
     //* ---------------------------------------------------------------------------------
 
     console.log(`After SUBMIT--> День:${day}, Месяц:${month}, Год:${year}`); //!
-    console.log("After SUBMIT--> dateOfBirth:", dateOfBirth); //!
+    console.log("After SUBMIT--> dateOfBirthObj:", dateOfBirthObj); //!
 
     console.log("firstAddNumber I:", firstAddNumber); //!
     console.log("secondAddNumber II:", secondAddNumber); //!
